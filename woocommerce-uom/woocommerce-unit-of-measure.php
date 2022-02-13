f9525cd699afec7eaff94f77a682eb20b20bab4d
Haziq Hapiz <zyqq.dev@gmail.com>
2022-02-13 22:20:13 +0800

    update

--- woocommerce-uom/woocommerce-unit-of-measure.php
+++ woocommerce-uom/woocommerce-unit-of-measure.php
@@ -1,23 +1,18 @@
 <?php
 /**
- * Plugin Name: WooCommerce Unit Of Measure
+ * Plugin Name: WooCommerce UOM
  * Plugin URI:
- * Description: WooCommerce Unit Of Measure allows the user to add a unit of measure (or any text) after the price on WooCommerce products.
- * Version: 3.0.2
- * Author: Bradley Davis
- * Author URI: http://bradley-davis.com
+ * Description: WooCommerce UOM allows the user to add a unit of measure (or any text) after the price on WooCommerce products.
+ * Version: 1.0.1
+ * Author: Haziq Hapiz
+ * Author URI: https://haziqhapiz.me
  * License: GPL3
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  * Text Domain: woocommerce-uom
  * WC requires at least: 3.0.0
  * WC tested up to: 5.1.0
  *
- * @author    Bradley Davis
- * @category  Admin
- * @package   WooCommerce RRP
- * @since     1.0.0
- *
- * WooCommerce Unit Of Measure. A Plugin that works with the WooCommerce plugin for WordPress.
+ * WooCommerce UOM. A Plugin that works with the WooCommerce plugin for WordPress.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
